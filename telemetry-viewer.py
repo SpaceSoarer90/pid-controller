27912fb703e5de2d36d02e829c5e8dd00d975895
SpaceSoarer90 <josequintusxavier@gmail.com>
2026-01-13 15:34:48 +0800

    removed irrelevant files

--- telemetry-viewer.py
+++ telemetry-viewer.py
@@ -1,9 +1,88 @@
-from enum import IntEnum
 import matplotlib.pyplot as plt
 import serial
 import time
+from enum import IntEnum
 
-# this is just an enum class
+plt.ion()
+
+DATAPLOT = 0
+METERTEXT = 1
+
+SYSTEMPLOT = 0
+TERMPLOT = 1
+
+METER = 0
+TEXT = 1
+
+UNSCALED = 0
+SCALED = 1
+
+fig = plt.figure(layout='constrained')
+subfig = fig.subfigures(1, 2, width_ratios=[5, 1])
+dataplot_fig = subfig[DATAPLOT].subfigures(2, 1, height_ratios=[1, 1])
+metertext_fig = subfig[METERTEXT].subfigures(2, 1, height_ratios=[2, 1])
+
+########################################################
+dataplot_fig[SYSTEMPLOT].suptitle('System Plot')
+dataplot_fig[TERMPLOT].suptitle('PID Terms Plot')
+
+sys_ax = dataplot_fig[SYSTEMPLOT].subplots(1, 1)
+term_ax = dataplot_fig[TERMPLOT].subplots(1, 2)
+
+sys_plots = sys_ax.plot([], [], 'b-', [], [], 'g-', [], [], 'r-', [], [], 'y-')
+unscaled_term_plots = term_ax[UNSCALED].plot([], [], 'b-', [], [], 'r-', [], [], 'g-')
+scaled_term_plots = term_ax[SCALED].plot([], [], 'b-', [], [], 'r-', [], [], 'g-')
+
+sys_ax.set_title('Setpoint, Currpoint, Error, Output')
+term_ax[UNSCALED].set_title('Unscaled')
+term_ax[SCALED].set_title('Scaled')
+
+MIN_OUTPUT, MAX_OUTPUT = 0, 400
+sys_ax.set_xlabel('Time (s)')
+sys_ax.set_ylabel('Speed (RPM)')
+sys_ax.set_ylim(MIN_OUTPUT - 50, MAX_OUTPUT + 50)
+
+sys_ax.legend(['Setpoint', 'Currpoint', 'Error', 'Output'])
+term_ax[UNSCALED].legend(['Proportional', 'Integral', 'Derivative'])
+term_ax[SCALED].legend(['Proportional', 'Integral', 'Derivative'])
+########################################################
+
+########################################################
+OUTPUT = 0
+PWM_OUTPUT = 1
+
+MIN_PWM_OUTPUT = 0
+MAX_PWM_OUTPUT = 255
+
+meter_data = {
+    'output': 20.0,
+    'pwm_output': 255
+}
+meter_data_tuple = tuple(meter_data.values())
+
+metertext_fig[METER].suptitle('Output Meters')
+metertext_fig[TEXT].suptitle('Textual Telemetry')
+
+outmeter = metertext_fig[METER].subplots(1, 2)
+bar1 = outmeter[OUTPUT].bar('OUTPUT', meter_data['output'])[0]
+outmeter[OUTPUT].set_ylim(MIN_OUTPUT, MAX_OUTPUT)
+
+bar2 = outmeter[PWM_OUTPUT].bar('PWM OUTPUT', meter_data['pwm_output'])[0]
+outmeter[PWM_OUTPUT].set_ylim(MIN_PWM_OUTPUT, MAX_PWM_OUTPUT)
+
+outmeters = [bar1, bar2]
+
+########################################################
+
+device = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+time.sleep(2.0)
+
+print('Sending RPM Command to device!')
+device.write(b'r')
+time.sleep(0.1)
+device.write(b'200!')
+
+########################################################
 class DataColumn(IntEnum):
     TIMESTAMP = 0
 
@@ -13,100 +92,106 @@ class DataColumn(IntEnum):
     OUTPUT = 4
 
     RAW_OUTPUT = 5
-    P_TERM = 6
-    I_TERM = 7
-    D_TERM = 8
-    KP = 9
-    KI = 10
-    KD = 11
-    DT = 12
+    PROPORTIONAL = 6
+    INTEGRAL = 7
+    DERIVATIVE = 8
+    P_TERM = 9
+    I_TERM = 10
+    D_TERM = 11
+    KP = 12
+    KI = 13
+    KD = 14
+    DT = 15
 
-    PWM_OUTPUT = 13
-    SAMPLING_INTERVAL = 14
-    CALCULATION_TIME = 15
-    SATURATED = 16
+    PWM_OUTPUT = 16
+    SAMPLING_INTERVAL = 17
+    CALCULATION_TIME = 18
+    SATURATED = 19
 
-MIN_Y_AXIS = -300
-MAX_Y_AXIS = 300
-
-plt.ion()
-
-fig, ax = plt.subplots()
-
-
-# disable autofocus
-if hasattr(fig.canvas.manager, 'raise_window'):
-    fig.canvas.manager.raise_window = lambda *a, **k: None
+########################################################
 
 data = {
     'timestamp': [],
     'setpoint': [],
     'currpoint': [],
     'error': [],
-    'output': []
+    'output': [],
+
+    'proportional': [],
+    'integral': [],
+    'derivative': [],
+    'p_term': [],
+    'i_term': [],
+    'd_term': [],
+    'kp': 0,
+    'ki': 0,
+    'kd': 0,
+
+    'pwm_output': []
 }
-# create two 2d plots with their datas empty initially
-# make the color of the first blue and the second green
-plots = ax.plot([], [], 'b-', [], [], 'g-', [], [], 'r-', [], [], 'y-')
 
-ax.set_xlabel('Time (s)')
-ax.set_ylabel('RPM')
-ax.set_title('Setpoint, Currpoint, Error, Output')
-ax.set_ylim(MIN_Y_AXIS, MAX_Y_AXIS)
-
-device = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-
-time.sleep(2)
-
-print("sending to device!")
-device.write(b'r')
-time.sleep(0.1) #yes, i know it sucks... bear with me pls
-device.write(b'100!')
-
-update_plot_time = 1.0 #default value
-
-plots[0].set_zorder(len(plots))
+update_plot_time = 1.0
 
 while True:
     if device.in_waiting > 0:
         raw_data_line = device.readline().decode('ascii').strip()
 
         if raw_data_line[0] != '#':
-            print(raw_data_line)
+            print(f"not data: {raw_data_line}")
             continue
-
+        
         data_frame = raw_data_line.lstrip('# ').split(',')
         data_frame = [float(data) for data in data_frame]
-
-        # print(data_frame[DataColumn.SETPOINT],
-        #       data_frame[DataColumn.CURRPOINT],
-        #       data_frame[DataColumn.ERROR],
-        #       data_frame[DataColumn.OUTPUT])
-        print(data_frame[-1]) # print the delta pulses
 
         data['timestamp'].append(data_frame[DataColumn.TIMESTAMP])
         data['setpoint'].append(data_frame[DataColumn.SETPOINT])
         data['currpoint'].append(data_frame[DataColumn.CURRPOINT])
         data['error'].append(data_frame[DataColumn.ERROR])
         data['output'].append(data_frame[DataColumn.OUTPUT])
+
+        data['proportional'].append(data_frame[DataColumn.PROPORTIONAL])
+        data['integral'].append(data_frame[DataColumn.INTEGRAL])
+        data['derivative'].append(data_frame[DataColumn.DERIVATIVE])
+        data['p_term'].append(data_frame[DataColumn.P_TERM])
+        data['i_term'].append(data_frame[DataColumn.I_TERM])
+        data['d_term'].append(data_frame[DataColumn.D_TERM])
+
+        data['kp'] = data_frame[DataColumn.KP]
+        data['ki'] = data_frame[DataColumn.KI]
+        data['kd'] = data_frame[DataColumn.KD]
+        data['pwm_output'].append(data_frame[DataColumn.PWM_OUTPUT])
+
+        plot_list_list = [sys_plots[:], unscaled_term_plots[:], scaled_term_plots[:]]
+
+        for plot_list in plot_list_list:
+            for plot in plot_list:
+                plot.set_xdata(data['timestamp'])
+
+        sys_plots[0].set_ydata(data['setpoint'])
+        sys_plots[1].set_ydata(data['currpoint'])
+        sys_plots[2].set_ydata(data['error'])
+        sys_plots[3].set_ydata(data['output'])
+
+        unscaled_term_plots[0].set_ydata(data['proportional'])
+        unscaled_term_plots[1].set_ydata(data['integral'])
+        unscaled_term_plots[2].set_ydata(data['derivative'])
+
+        scaled_term_plots[0].set_ydata(data['p_term'])
+        scaled_term_plots[1].set_ydata(data['i_term'])
+        scaled_term_plots[2].set_ydata(data['d_term'])
+
+        outmeters[OUTPUT].set_height(data['output'][-1])
+        outmeters[PWM_OUTPUT].set_height(data['pwm_output'][-1])
+
         update_plot_time = data_frame[DataColumn.SAMPLING_INTERVAL]
 
-        # i need to change this
-        for plot in plots:
-            plot.set_xdata(data['timestamp'])
+        sys_ax.relim()
+        sys_ax.autoscale_view()
 
-        plots[0].set_ydata(data['setpoint'])
-        plots[1].set_ydata(data['currpoint'])
-        plots[2].set_ydata(data['error'])
-        plots[3].set_ydata(data['output'])
-
-
-        ax.relim()
-        ax.autoscale_view()
-
-        # no need for plt.pause since this only runs when there's new data
+        term_ax[UNSCALED].relim()
+        term_ax[UNSCALED].autoscale_view()
+        term_ax[SCALED].relim()
+        term_ax[SCALED].autoscale_view()
 
     fig.canvas.draw_idle()
     fig.canvas.start_event_loop(update_plot_time / 2.0)
-
-    pass
